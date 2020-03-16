8e8ce82a76821241e46395e946a54c6ce399f809
S8A <samuelochoap@gmail.com>
2020-03-16 12:24:08 -0400

    Creación de __main__ y ejemplo de archivo de config.

--- umc_crud/__main__.py
+++ umc_crud/__main__.py
@@ -1,4 +1,15 @@
-from umc_crud import login
+from .login import login
+from .config import config
+import argparse
+
+def main(args):
+	if args['config']:
+		config()
+	else:
+		login()
 
 if __name__ == '__main__':
-	login.login()
+	parser = argparse.ArgumentParser(description='Sistema CRUD de campus UMC.')
+	parser.add_argument('--config', action='store_true')
+	args = parser.parse_args()
+	main(vars(args))
