594dc6c427665d132382b01f1308aa9416b73cc0
S8A <samuelochoap@gmail.com>
2020-03-16 17:31:08 -0400

    Módulo de inicio de sesión completo, nueva función execute_sql

--- umc_crud/db.py
+++ umc_crud/db.py
@@ -2,7 +2,7 @@ import pymysql
 
 # Módulo de manejo de base datos
 
-def connect_to_db(mysql_conf):
+def connect(mysql_conf):
     """Crea una conexión a la base de datos indicada en la configuración."""
     return pymysql.connect(host=mysql_conf['host'],
                            user=mysql_conf['user'],
@@ -10,3 +10,30 @@ def connect_to_db(mysql_conf):
                            database=mysql_conf['database'],
                            charset='utf8',
                            cursorclass=pymysql.cursors.DictCursor)
+
+
+def execute_sql(mysql_conf, query, args=None, rows=None):
+    """
+    Ejecuta una petición SQL y devuelve su resultado.
+
+    Argumentos:
+    mysql_conf (dict) -- Configuración de conexión a la base de datos
+    query (str) -- Petición a realizar
+    args (tuple/list/dict) --  Parámetros de la petición (opc)
+    rows (int or None) -- Número de filas de resultado (opc).
+    """
+    connection = connect(mysql_conf)
+    result = None
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query, args)
+            if rows is None:
+                result = cursor.fetchall()
+            elif rows == 1:
+                result = cursor.fetchone()
+            elif rows > 1:
+                result = cursor.fetchmany(size=rows)
+        connection.commit()
+    finally:
+        connection.close()
+    return result
--- umc_crud/login.py
+++ umc_crud/login.py
@@ -1,2 +1,47 @@
+from .cli import print_h1, print_h2, print_long, print_error
+from .config import read_config
+from .db import execute_sql
+
+# Módulo de inicio de sesión
+
 def login():
-	print('Logging in')
+    """Permite iniciar sesión como un usuario del campus UMC."""
+    conf = read_config()
+    if 'mysql' not in conf.sections():
+        # Muestra un error si no se encuentra la configuración de MySQL
+        print_error('Archivo de configuración no encontrado o incompleto. '
+                    'Ejecutar de nuevo usando la opción --config para '
+                    'crear un nuevo archivo de configuración.')
+    else:
+        # Petición de búsqueda en la tabla usuario
+        query = 'SELECT * FROM usuario WHERE id = %s AND password = %s'
+
+        print_h1('Inicio de Sesión')
+        # Pregunta los datos de inicio de sesión de forma repetitiva
+        while True:
+            user_id = input('Usuario: ')
+            user_pw = input('Contraseña: ')
+            print()
+            # Ejecuta la búsqueda por usuario y contraseña
+            result = execute_sql(conf['mysql'],
+                                 query,
+                                 args=(user_id, user_pw),
+                                 rows=1)
+            if result is not None:
+                # Si los datos coinciden con un usuario, se verifica si es 
+                # un administrador o un estudiante y se ejecuta el módulo 
+                # apropiado.
+                if result['admin'] == 0:
+                    print('Estudiante')
+                    #umc_crud.student.main()
+                else:
+                    print('Administrador')
+                    #umc_crud.admin.main()
+
+                # Se rompe el bucle
+                break
+            else:
+                # Si los datos no coinciden con ningún usuario, se muestra
+                # un mensaje de error.
+                print_error('Usuario o contraseña incorrecta. '
+                            'Intente de nuevo.')
