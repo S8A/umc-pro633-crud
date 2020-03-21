da9c06fa1a2b003057d6028a738e205dc7321886
S8A <samuelochoap@gmail.com>
2020-03-21 15:56:51 -0400

    Agregados comentarios explicativos

--- umc_crud/admin.py
+++ umc_crud/admin.py
@@ -12,7 +12,7 @@ def main(user_id):
         ],
         'Registro': [
             ['Registrar calificaciones manualmente', make_records],
-            ['Cargar archivo de calificaciones', csv_records]
+            ['Cargar archivo de calificaciones', load_csv_records]
         ],
         'Modificación': [
             ['Modificar calificaciones', update_record]
@@ -60,12 +60,16 @@ def get_personal_info(title=True, intro=True):
         io.print_long('Ingrese uno o varios números de C.I. para buscar '
                       'la información personal de los estudiantes a los '
                       'que correspondan.')
+    # Pedir los números de cédula y buscar los estudiantes asociados
     estudiantes = crud.find_students(io.input_list('Cédula(s): '))
     if not estudiantes:
+        # Si la lista de estudiantes está vacía, mostrar error
         io.print_error('Las C.I. ingresadas no corresponden a ningún '
                        'estudiante.')
     else:
+        # De lo contrario, para cada estudiante
         for estudiante in estudiantes:
+            # Mostrar la información personal del estudiante
             io.print_h3(estudiante['id_usuario'], newline=False)
             student.get_personal_info(estudiante, title=False)
 
@@ -78,12 +82,16 @@ def get_record(title=True, intro=True):
         io.print_long('Ingrese uno o varios números de C.I. para buscar '
                       'los récords académicos de los estudiantes a los que '
                       'correspondan.')
+    # Pedir los números de cédula y buscar los estudiantes asociados
     estudiantes = crud.find_students(io.input_list('Cédula(s): '))
     if not estudiantes:
+        # Si la lista de estudiantes está vacía, mostrar error
         io.print_error('Las C.I. ingresadas no corresponden a ningún '
                        'estudiante.')
     else:
+        # De lo contrario, para cada estudiante
         for estudiante in estudiantes:
+            # Mostrar el récord académico del estudiante
             io.print_h3(f'{estudiante["id_usuario"]} : {estudiante["ci"]}')
             student.get_record(estudiante, title=False)
 
@@ -97,13 +105,18 @@ def find_grades(title=True, intro=True):
                       'códigos de materia para buscar las califaciones de '
                       'cada estudiante en cada una de esas materias que '
                       'haya cursado.')
+    # Pedir los números de cédula y buscar los estudiantes asociados
     estudiantes = crud.find_students(io.input_list('Cédula(s): '))
+    # Pedir las materias a consultar
     materia_ids = [m.upper() for m in io.input_list('Materia(s): ')]
     if not estudiantes:
+        # Si la lista de estudiantes está vacía, mostrar error
         io.print_error('Las C.I. ingresadas no corresponden a ningún '
                        'estudiante.')
     else:
+        # De lo contrario, para cada estudiante
         for estudiante in estudiantes:
+            # Mostrar el récord académico del estudiante en las materias dadas
             io.print_h3(f'{estudiante["id_usuario"]} : {estudiante["ci"]}')
             student.print_record(crud.read_records(estudiante['ci'],
                                                    materia_ids))
@@ -117,52 +130,72 @@ def make_records(title=True, intro=True):
         io.print_long('Ingrese uno o varios números de C.I., las materias '
                       'que va a registrar para cada estudiante, y la '
                       'calificación obtenida en cada una de ellas.')
+    # Pedir los números de cédula y buscar los estudiantes asociados
     estudiantes = crud.find_students(io.input_list('Cédula(s): '))
     if not estudiantes:
+        # Si la lista de estudiantes está vacía, mostrar error
         io.print_error('Las C.I. ingresadas no corresponden a ningún '
                        'estudiante.')
     else:
+        # De lo contrario, para cada estudiante
         for estudiante in estudiantes:
+            # Mostrar el usuario y cédula del estudiante
             io.print_h3(f'{estudiante["id_usuario"]} : {estudiante["ci"]}')
+            # Buscar las materias cursadas por el estudiante
             cursadas = [r['id'] for r in crud.read_records(estudiante['ci'])]
+            # Pedir el período académico en el que se registrarán los datos
             periodo = io.input_period('Período académico: ')
+            # Pedir las materias que se registrarán y buscarlas en la
+            # base de datos
             materias = crud.find_career_subjects(
                 estudiante['id_carrera'],
                 [m.upper() for m in io.input_list('Materia(s): ')])
+            # Columnas de la tabla y sus cabeceras
             cols = {'ci_estudiante': 'Cédula',
                     'id_materia': 'Materia',
                     'nota': 'Nota',
                     'periodo': 'Período'}
+            # Lista de datos a registrar
             por_registrar = []
             print('Calificaciones:')
+            # Para cada materia válida a registrar
             for materia in materias:
+                # Código de la materia
                 materia_id = materia['id_materia']
                 if materia_id in cursadas:
+                    # Si la materia ya fue cursada, mostrar mensaje
                     print(f'- {materia_id} ya tiene calificación.')
                 else:
+                    # Si no ha sido cursada, pedir su calificación
                     nota = io.input_int(f'- {materia_id}: ', newline=False)
+                    # Agregar datos de la materia a registrar a la lista
                     por_registrar.append({'ci_estudiante': estudiante['ci'],
                                           'id_materia': materia_id,
                                           'nota': nota,
                                           'periodo': periodo})
             print()
             if por_registrar:
+                # Si hay datos por registrar, mostrarlos en una tabla
                 print('Se registrarán los siguientes datos:')
                 io.print_table(por_registrar, cols)
+                # Pedir confirmación antes de registrar los datos
                 confirm = io.input_yes_no('¿Registrar datos? (s/n): ')
                 if confirm:
+                    # Si la respuesta es afirmativa, crear los registros
                     crud.create_records(por_registrar)
                     print('Datos registrados exitosamente.')
                 else:
+                    # De lo contrario, mostrar mensaje
                     print('Registro cancelado.')
             else:
+                # Si no hay datos por registrar, mostrar mensaje
                 print('No se registrarán datos.')
             print()
 
 
-def csv_records():
+def load_csv_records():
     """Registra calificaciones de varios estudiantes a partir de un CSV."""
-    print('TODO: csv_records')
+    print('TODO: load_csv_records')
 
 
 def update_record():
