d7622bed9954ccaf40f87eb9689d8f837dbefff0
S8A <samuelochoap@gmail.com>
2020-04-01 13:33:12 -0400

    Primer prototipo de la función de consulta de récord académico

--- umc_crud/gui/student.py
+++ umc_crud/gui/student.py
@@ -1,4 +1,5 @@
 from .. import crud
+from ..student import calculate_ia, print_record
 from . import utils
 import PyQt5.QtCore as qtc
 import PyQt5.QtWidgets as qtw
@@ -19,8 +20,8 @@ class MainWindow(qtw.QMainWindow):
         """Crea la interfaz gráfica de la ventana."""
         # Crea la barra de herramientas
         self._create_main_toolbar()
-        # Crea un widget vacío para llenar el área central
-        self.setCentralWidget(qtw.QWidget())
+        # Inicia mostrando la información del estudiante
+        self._get_personal_info()
         self.setMinimumSize(400, 300)
 
     def _create_main_toolbar(self):
@@ -71,6 +72,66 @@ class MainWindow(qtw.QMainWindow):
             layout.addWidget(qtw.QLabel(f'<b>Mención:</b> {mencion}'))
         widget.setLayout(layout)
         self.setCentralWidget(widget)
+        self.resize(400, 300)
 
     def _get_record(self):
-        print('TODO: _get_record')
+        """Consulta el récord académico del estudiante."""
+        # Crea una interfaz de consulta de récord académico para
+        # un solo estudiante
+        self.setCentralWidget(StudentRecordWidget(student=self.estudiante))
+        self.resize(500, 500)
+
+
+class StudentRecordWidget(qtw.QWidget):
+    """Interfaz de consulta de récord académico."""
+
+    def __init__(self, student=None):
+        """Inicialización."""
+        super().__init__()
+        self.record = {}
+        if student is not None:
+            self.single = True
+            self.estudiante = student
+        self._create_ui()
+        self._find_record()
+
+    def _create_ui(self):
+        """Crea la interfaz gráfica de consulta de récord académico."""
+        # Estructura
+        main_widget = qtw.QWidget()
+        main_layout = qtw.QVBoxLayout()
+        # Cabecera
+        main_layout.addWidget(utils.create_label_h1('Récord académico'))
+        # Formulario de búsqueda
+        form_layout = qtw.QFormLayout()
+        if not self.single:
+            self.estudiante_input = qtw.QLineEdit()
+            form_layout.addRow('Estudiante', self.estudiante_input)
+        self.materias_input = qtw.QLineEdit()
+        form_layout.addRow('Materias', self.materias_input)
+        self.periodo_input = qtw.QLineEdit()
+        form_layout.addRow('Período', self.periodo_input)
+        main_layout.addLayout(form_layout)
+        consultar_btn = qtw.QPushButton('Consultar')
+        consultar_btn.clicked.connect(self._find_record)
+        main_layout.addWidget(consultar_btn)
+        # Tabla de datos
+        # TODO: Convertir la tabla a QTableView
+        self.record_tbl = qtw.QTextEdit()
+        self.record_tbl.setReadOnly(True)
+        main_layout.addWidget(self.record_tbl, stretch=2)
+        # Información adicional
+        uc_cursadas = [r['uc'] for r in self.record]
+        main_layout.addWidget(utils.create_label(
+            f'Materias cursadas: {len(uc_cursadas)} ({sum(uc_cursadas)} UC)'))
+        uc_aprobadas = [r['uc'] for r in self.record if r['nota'] >= 12]
+        main_layout.addWidget(utils.create_label(
+            f'Materias aprobadas: {len(uc_aprobadas)} '
+            f'({sum(uc_aprobadas)} UC)'))
+        main_layout.addWidget(utils.create_label(
+            f'Índice Académico Acumulado (IAA): {calculate_ia(self.record)}'))
+        self.setLayout(main_layout)
+
+    def _find_record(self):
+        """Consulta el récord académico con los datos ingresados."""
+        print('TODO: _find_record')
